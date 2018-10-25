0fd48e03e656157204a2618fe5f61e55c087fda9
inorinne <inorinne@gmail.com>
2018-10-25 17:37:08 +0800

    trying to use scrape twitter followers

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn monitor_followers.wsgi --log-file -
---
+++ monitor_followers/views.py
@@ -0,0 +1,33 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from requests_html import HTMLSession
+
+def index(request):
+    template_name = 'index.html'
+    search = [
+        'iwakura_azusa',
+        'eririn959',
+        'vampiretoricago',
+    ]
+
+    r = []
+    context = {}
+
+    for x in search:
+        r.append(get_followers(x))
+    
+    context['title']        = 'Monitor Followers'
+    context['twitter_info'] = r
+    return render(request, template_name, context)
+
+def get_followers(twitter_username):
+    r = {}
+    with HTMLSession() as s:
+        r = s.get('https://twitter.com/{!s}'.format(twitter_username))
+        followers_count = r.html.find('#page-container > div.ProfileCanopy.ProfileCanopy--withNav.ProfileCanopy--large.js-variableHeightTopBar > div > div.ProfileCanopy-navBar.u-boxShadow > div.AppContainer > div > div.Grid-cell.u-size2of3.u-lg-size3of4 > div > div > ul > li.ProfileNav-item.ProfileNav-item--followers > a > span.ProfileNav-value', first=True)
+        f = followers_count.text
+        r = {
+            'username': twitter_username,
+            'followers': f
+        }
+        return r
